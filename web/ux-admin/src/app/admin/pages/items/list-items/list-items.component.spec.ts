10df516d32ac24bae47184104c88437fa37cec89
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-02-07 23:45:19 +0400

    Fixes #1 :REGISTER NEW COMPANY

--- web/ux-admin/src/app/admin/pages/items/list-items/list-items.component.spec.ts
+++ web/ux-admin/src/app/admin/pages/items/list-items/list-items.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ListItemsComponent } from './list-items.component';
+import {ListItemsComponent} from './list-items.component';
 
 describe('ListItemsComponent', () => {
   let component: ListItemsComponent;
@@ -8,9 +8,9 @@ describe('ListItemsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListItemsComponent ]
+      declarations: [ListItemsComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
