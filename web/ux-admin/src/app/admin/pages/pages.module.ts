a89b24bbefac106b56145d7d88b6a55e0f669d4e
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-02-07 23:19:36 +0400

    Fixes #1 :REGISTER NEW COMPANY

--- web/ux-admin/src/app/admin/pages/pages.module.ts
+++ web/ux-admin/src/app/admin/pages/pages.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { PagesRoutingModule } from './pages-routing.module';
-import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {PagesRoutingModule} from './pages-routing.module';
+import {MainLayoutComponent} from './main-layout/main-layout.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ClarityModule} from '@clr/angular';
 
 @NgModule({
   imports: [
     CommonModule,
+    ClarityModule,
     PagesRoutingModule
   ],
   declarations: [MainLayoutComponent, DashboardComponent]
 })
-export class PagesModule { }
+export class PagesModule {
+}
