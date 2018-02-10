9a2c65edc3df1e9b0c14e5fd771fbbedb7bb83b1
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-02-10 12:56:28 +0400

    final

--- web/ux-admin/src/app/admin/pages/items/list-items/list-items.component.spec.ts
+++ web/ux-admin/src/app/admin/pages/items/list-items/list-items.component.spec.ts
@@ -1,21 +1,24 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ListItemsComponent} from './list-items.component';
+import {ClarityModule} from '@clr/angular';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RetrieveItemsUseCaseModule} from '../../../../domain/items/usecase/list-items';
+import {By} from '@angular/platform-browser';
 
-describe('ListItemsComponent', () => {
+describe('[ListItemsComponent] Unit Tests', () => {
   let component: ListItemsComponent;
   let fixture: ComponentFixture<ListItemsComponent>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ListItemsComponent]
-    })
-      .compileComponents();
+    ListItemsComponentUnitTests.setup().then(() => {
+      fixture = TestBed.createComponent(ListItemsComponent);
+      component = fixture.componentInstance;
+    });
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ListItemsComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
@@ -23,7 +26,24 @@ describe('ListItemsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it(`should render list of items on grid`, async(() => {
-
+  it(`should render list of items on grid has need columns`, async(() => {
+    fixture.debugElement.query(By.css('table'));
   }));
 });
+
+namespace ListItemsComponentUnitTests {
+
+  export function setup() {
+    return TestBed.configureTestingModule({
+      imports: [
+        ClarityModule,
+        NoopAnimationsModule,
+        HttpClientTestingModule
+      ],
+      declarations: [ListItemsComponent],
+      providers: [RetrieveItemsUseCaseModule.providers()]
+    })
+      .compileComponents();
+  }
+
+}
